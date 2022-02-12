5d752eb5d7c1a4e2ea6c903262fc009b9d050fc3
Romulo-Moraes <romuloperes6@gmail.com>
2022-02-12 16:46:46 -0300

    Section selection added

--- sysinfo.cpp
+++ sysinfo.cpp
@@ -9,145 +9,219 @@
 #include <vector>
 #include <filesystem>
 #include <cctype>
+#include "libs/argsParser.hpp"
 
 using namespace std;
 namespace fs = std::filesystem;
+using namespace argparse;
 
 #include "helpers/programInteraction.hpp"
 
+int main(int argc, char *argv[])
+{
 
-
-
-int main(int argc,char *argv[]){
-	colorizedPrint(foreColor::fGreen, backColor::bNone,"================= User =================\n");
-	passwd *userDatabase = getpwuid(getuid());
-	cout << "User name: ";
-	colorizedPrint(foreColor::fNone,backColor::bNone,"%s\n",{userDatabase->pw_name});
-	cout << "User directory: ";
-	colorizedPrint(foreColor::fNone,backColor::bNone,"%s\n",{userDatabase->pw_dir});
-	cout << "User default shell: ";
-	colorizedPrint(foreColor::fNone,backColor::bNone,"%s\n",{userDatabase->pw_shell});
-
-	cout << endl;
-
-	colorizedPrint(foreColor::fGreen, backColor::bNone,"================ Hardware ================\n");
-
-	fstream memoryDatabase;
-	memoryDatabase.open("/proc/meminfo", ios::in);
+	bool anySectionSelected = false;
+	bool userSectionSelected = false;
+	bool cpuSectionSelected = false;
+	bool ramSectionSelected = false;
+	bool osSectionSelected = false;
 
 	string lineFromFile;
 
-	pair<string,string> keyAndValueFromInfo;
+	pair<string, string> keyAndValueFromInfo;
 	string key;
 	string value;
 
-	if(memoryDatabase.is_open()){
-		int valueToConvert = 0;
-		while(getline(memoryDatabase,lineFromFile)){
-			keyAndValueFromInfo = getKeyAndValue(lineFromFile);
-			if(keyAndValueFromInfo.first == "MemTotal" || keyAndValueFromInfo.first == "MemAvailable" || keyAndValueFromInfo.first == "MemFree"){
-				keyAndValueFromInfo.second = removeAllNotNumbers(removeAllSpaces(keyAndValueFromInfo.second));
-				valueToConvert = convertStringToNumber(keyAndValueFromInfo.second);
-				valueToConvert = convertKbToMb(valueToConvert);
-				cout << keyAndValueFromInfo.first << ": ";
-				colorizedPrint(foreColor::fNone,backColor::bNone,"%s MB\n",{to_string(valueToConvert)});
-			}
-		}	
+	ArgumentParser parser("SysInfo", "1.0");
+
+	parser.add_argument("-c", "--cpu").default_value("-").help("Print the CPU section");
+	parser.add_argument("-r", "--ram").default_value("-").help("Print the RAM section");
+	parser.add_argument("-u", "--user").default_value("-").help("Print the USER section");
+	parser.add_argument("-o", "--os").default_value("-").help("Print the OS section");
+
+	try
+	{
+		parser.parse_args(argc, argv);
 	}
-	else{
-		colorizedPrint(foreColor::fRed, backColor::bNone,"Error in open memory database. Skip\n \n");
+	catch (runtime_error &err)
+	{
+		cout << parser;
+		exit(1);
 	}
 
-	memoryDatabase.close();
+	if (parser.is_used("-c") || parser.is_used("-r") || parser.is_used("-u") || parser.is_used("-o"))
+	{
 
-	cout << endl;
-
-	fstream cpuDatabase;
-	cpuDatabase.open("/proc/cpuinfo", ios::in);
-
-	if(cpuDatabase.is_open()){
-		int numberOfProcessors = 0;
-		vector<pair<int,string>> MHzOfEachProcessor;
-		string cpuCores = "";
-		string modelName = "";
-		string cacheSize = "";
-
-		while(getline(cpuDatabase,lineFromFile)){
-			keyAndValueFromInfo = getKeyAndValue(lineFromFile);
-			keyAndValueFromInfo.first = removeAllSpaces(keyAndValueFromInfo.first);
-			key = keyAndValueFromInfo.first;
-			value = keyAndValueFromInfo.second;
-			if(key == "cpucores" || key == "processor" || key == "modelname" || key == "cachesize"){
-				if(key == "processor"){
-					numberOfProcessors += 1;
-				}
-				if(key == "cpucores"){
-					cpuCores = value;
-				}
-				if(key == "modelname"){
-					modelName = value;
-				}
-				if(key == "cachesize"){
-					cacheSize = value;
-				}
-			}
-
+		anySectionSelected = true;
+		if (parser.is_used("-c"))
+		{
+			cpuSectionSelected = true;
 		}
-
-		string beginPartOfPath = "/sys/devices/system/cpu/cpu";
-		string endPartOfPath = "/cpufreq/scaling_cur_freq";
-		string completePath = "";
-		string frequencyOfProcessor = "";
-		for(int i = 0; i < numberOfProcessors;i++){
-			completePath = beginPartOfPath;
-			completePath += to_string(i);
-			completePath += endPartOfPath;
-
-			fstream freqFile;
-			freqFile.open(completePath,ios::in);
-
-			if(freqFile.is_open()){
-				getline(freqFile,frequencyOfProcessor);
-				MHzOfEachProcessor.push_back(make_pair(i,frequencyOfProcessor));
-			}
-			else{
-				cout << "cant open file";
-			}
-
+		if (parser.is_used("-r"))
+		{
+			ramSectionSelected = true;
 		}
-
-		// Print all got informations
-		cout << "Cpu cores: ";
-		colorizedPrint(foreColor::fNone,backColor::bNone,"%s\n",{cpuCores});
-		cout << "Number of threads: ";
-		colorizedPrint(foreColor::fNone,backColor::bNone,"%s\n",{to_string(numberOfProcessors)});
-		cout << "Cpu model name: ";
-		colorizedPrint(foreColor::fNone,backColor::bNone,"%s\n",{modelName});
-		cout << "Cache size: ";
-		colorizedPrint(foreColor::fNone,backColor::bNone,"%s\n",{cacheSize});
-		cout << "Processors frequency: " << endl;
-		for(auto cpuFrequency : MHzOfEachProcessor){
-			cout << "       " << cpuFrequency.first << ": ";
-			colorizedPrint(foreColor::fNone,backColor::bNone,"%s GHz\n",{insertValueCommas(convertMhzToGhz(convertStringToNumber(cpuFrequency.second)))});
+		if (parser.is_used("-u"))
+		{
+			userSectionSelected = true;
+		}
+		if (parser.is_used("-o"))
+		{
+			osSectionSelected = true;
 		}
 	}
-	else{
-		colorizedPrint(foreColor::fRed, backColor::bNone,"Error in open cpu database. Skip\n \n");
+
+	if (!anySectionSelected || userSectionSelected)
+	{
+		colorizedPrint(foreColor::fGreen, backColor::bNone, "================= User =================\n");
+		passwd *userDatabase = getpwuid(getuid());
+		cout << "User name: ";
+		colorizedPrint(foreColor::fNone, backColor::bNone, "%s\n", {userDatabase->pw_name});
+		cout << "User directory: ";
+		colorizedPrint(foreColor::fNone, backColor::bNone, "%s\n", {userDatabase->pw_dir});
+		cout << "User default shell: ";
+		colorizedPrint(foreColor::fNone, backColor::bNone, "%s\n", {userDatabase->pw_shell});
+
+		cout << endl;
 	}
 
-	cpuDatabase.close();
+	if(ramSectionSelected || cpuSectionSelected || !anySectionSelected){
+		colorizedPrint(foreColor::fGreen, backColor::bNone, "================ Hardware ================\n");	
+	}
 
-	cout << endl << endl;
+	if (ramSectionSelected || !anySectionSelected)
+	{
+		fstream memoryDatabase;
+		memoryDatabase.open("/proc/meminfo", ios::in);
+
+		if (memoryDatabase.is_open())
+		{
+			int valueToConvert = 0;
+			while (getline(memoryDatabase, lineFromFile))
+			{
+				keyAndValueFromInfo = getKeyAndValue(lineFromFile);
+				if (keyAndValueFromInfo.first == "MemTotal" || keyAndValueFromInfo.first == "MemAvailable" || keyAndValueFromInfo.first == "MemFree")
+				{
+					keyAndValueFromInfo.second = removeAllNotNumbers(removeAllSpaces(keyAndValueFromInfo.second));
+					valueToConvert = convertStringToNumber(keyAndValueFromInfo.second);
+					valueToConvert = convertKbToMb(valueToConvert);
+					cout << keyAndValueFromInfo.first << ": ";
+					colorizedPrint(foreColor::fNone, backColor::bNone, "%s MB\n", {to_string(valueToConvert)});
+				}
+			}
+		}
+		else
+		{
+			colorizedPrint(foreColor::fRed, backColor::bNone, "Error in open memory database. Skip\n \n");
+		}
+
+		memoryDatabase.close();
+
+		cout << endl;
+	}
+
+	if (cpuSectionSelected || !anySectionSelected)
+	{
+		fstream cpuDatabase;
+		cpuDatabase.open("/proc/cpuinfo", ios::in);
+
+		if (cpuDatabase.is_open())
+		{
+			int numberOfProcessors = 0;
+			vector<pair<int, string>> MHzOfEachProcessor;
+			string cpuCores = "";
+			string modelName = "";
+			string cacheSize = "";
+
+			while (getline(cpuDatabase, lineFromFile))
+			{
+				keyAndValueFromInfo = getKeyAndValue(lineFromFile);
+				keyAndValueFromInfo.first = removeAllSpaces(keyAndValueFromInfo.first);
+				key = keyAndValueFromInfo.first;
+				value = keyAndValueFromInfo.second;
+				if (key == "cpucores" || key == "processor" || key == "modelname" || key == "cachesize")
+				{
+					if (key == "processor")
+					{
+						numberOfProcessors += 1;
+					}
+					if (key == "cpucores")
+					{
+						cpuCores = value;
+					}
+					if (key == "modelname")
+					{
+						modelName = value;
+					}
+					if (key == "cachesize")
+					{
+						cacheSize = value;
+					}
+				}
+			}
+
+			string beginPartOfPath = "/sys/devices/system/cpu/cpu";
+			string endPartOfPath = "/cpufreq/scaling_cur_freq";
+			string completePath = "";
+			string frequencyOfProcessor = "";
+			for (int i = 0; i < numberOfProcessors; i++)
+			{
+				completePath = beginPartOfPath;
+				completePath += to_string(i);
+				completePath += endPartOfPath;
+
+				fstream freqFile;
+				freqFile.open(completePath, ios::in);
+
+				if (freqFile.is_open())
+				{
+					getline(freqFile, frequencyOfProcessor);
+					MHzOfEachProcessor.push_back(make_pair(i, frequencyOfProcessor));
+				}
+				else
+				{
+					cout << "cant open file";
+				}
+			}
+
+			// Print all got informations
+			cout << "Cpu cores: ";
+			colorizedPrint(foreColor::fNone, backColor::bNone, "%s\n", {cpuCores});
+			cout << "Number of threads: ";
+			colorizedPrint(foreColor::fNone, backColor::bNone, "%s\n", {to_string(numberOfProcessors)});
+			cout << "Cpu model name: ";
+			colorizedPrint(foreColor::fNone, backColor::bNone, "%s\n", {modelName});
+			cout << "Cache size: ";
+			colorizedPrint(foreColor::fNone, backColor::bNone, "%s\n", {cacheSize});
+			cout << "Processors frequency: " << endl;
+			for (auto cpuFrequency : MHzOfEachProcessor)
+			{
+				cout << "       " << cpuFrequency.first << ": ";
+				colorizedPrint(foreColor::fNone, backColor::bNone, "%s GHz\n", {insertValueCommas(convertMhzToGhz(convertStringToNumber(cpuFrequency.second)))});
+			}
+		}
+		else
+		{
+			colorizedPrint(foreColor::fRed, backColor::bNone, "Error in open cpu database. Skip\n \n");
+		}
+		cpuDatabase.close();
+
+		cout << endl
+			 << endl;
+	}
 
 
-	colorizedPrint(foreColor::fGreen, backColor::bNone,"================ System ================\n");
+	if (!anySectionSelected || osSectionSelected)
+	{
+		colorizedPrint(foreColor::fGreen, backColor::bNone, "================ System ================\n");
 
-	utsname sysInfo;
-	uname(&sysInfo);
+		utsname sysInfo;
+		uname(&sysInfo);
 
-	cout << "Distro: " << sysInfo.release << endl;
-	cout << "OS kernel: " << sysInfo.sysname << endl;
-	cout << "Archtecture: " << sysInfo.machine << endl;
+		cout << "Distro: " << sysInfo.release << endl;
+		cout << "OS kernel: " << sysInfo.sysname << endl;
+		cout << "Archtecture: " << sysInfo.machine << endl;
+	}
 
 	return 0;
 }
